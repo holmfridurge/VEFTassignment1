ea155c0a7313ba0763ecb2bf7cc3e110bca93c68
holmfridurge <holmfridurge@gmail.com>
2016-08-26 20:19:57 +0000

    added required components to courses and students models

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -1,17 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication.Models
 {
     public class Course
     {
+        [Required]
         public string Name { get; set; }
         // example: "Web Services"
+        [Required]
         public string TemplateID { get; set; }
         // example: "T-514-VEFT"
+        [Required]
         public int ID { get; set; }
         // example: 1
+        [Required]
         public DateTime StartDate { get; set; }
         // example: "2016-08-17"
+        [Required]
         public DateTime EndDate { get; set; }
         // example: "2016-11-08"
     }
--- Models/Students.cs
+++ Models/Students.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication.Models
 {
     public class Student
     {
+        [Required]
         public int SSN { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
